3566e26a535836c2d978165e0e8019c1b944fecd
Paudric Smith <75310103+PaudricSmith@users.noreply.github.com>
2024-03-20 14:31:35 +0000

    Added the Categories table to the database

---
+++ BespokeWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using BespokeWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BespokeWeb.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
