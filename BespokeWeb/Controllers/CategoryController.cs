3e9606ecccd286353a525bdaa0603adb6bfcce6e
Paudric Smith <75310103+PaudricSmith@users.noreply.github.com>
2024-03-21 13:36:02 +0000

    Added Seed data to Categories table and a Category View table.

---
+++ BespokeWeb/Controllers/CategoryController.cs
@@ -0,0 +1,24 @@
+﻿using BespokeWeb.Data;
+using BespokeWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BespokeWeb.Controllers
+{
+    [Controller]
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public CategoryController(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+
+        public IActionResult Index()
+        {
+            List<Category> objCategoryList = _dbContext.Categories.ToList();
+            return View(objCategoryList);
+        }
+    }
+}
