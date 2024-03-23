400780ce90d98e59cc943949822e2b88cff66bef
Paudric Smith <75310103+PaudricSmith@users.noreply.github.com>
2024-03-23 11:55:37 +0000

    Added generic IRepository and Repository files

--- Bespoke.DataAccess/Migrations/ApplicationDbContextModelSnapshot.cs
+++ Bespoke.DataAccess/Migrations/ApplicationDbContextModelSnapshot.cs
@@ -21,7 +21,7 @@ namespace Bespoke.DataAccess.Migrations
 
             SqlServerModelBuilderExtensions.UseIdentityColumns(modelBuilder);
 
-            modelBuilder.Entity("BespokeWeb.Models.Category", b =>
+            modelBuilder.Entity("Bespoke.Models.Category", b =>
                 {
                     b.Property<int>("Id")
                         .ValueGeneratedOnAdd()
@@ -34,7 +34,8 @@ namespace Bespoke.DataAccess.Migrations
 
                     b.Property<string>("Name")
                         .IsRequired()
-                        .HasColumnType("nvarchar(max)");
+                        .HasMaxLength(30)
+                        .HasColumnType("nvarchar(30)");
 
                     b.HasKey("Id");
 
---
+++ Bespoke.DataAccess/Repository/Repository.cs
@@ -0,0 +1,50 @@
+﻿using Bespoke.DataAccess.Data;
+using Bespoke.DataAccess.Repository.IRepository;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace Bespoke.DataAccess.Repository
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly ApplicationDbContext _dbContext;
+        internal DbSet<T> dbSet;
+
+        public Repository(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+            dbSet = _dbContext.Set<T>();
+        }
+
+        
+        public void Add(T entity)
+        {
+            dbSet.Add(entity);
+        }
+
+        public T Get(Expression<Func<T, bool>> filter)
+        {
+            IQueryable<T> query = dbSet;
+            query = query.Where(filter);
+
+            return query.FirstOrDefault();
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            IQueryable<T> query = dbSet;
+
+            return query.ToList();
+        }
+
+        public void Remove(T entity)
+        {
+            dbSet.Remove(entity);
+        }
+
+        public void RemoveRange(IEnumerable<T> entities)
+        {
+            dbSet.RemoveRange(entities);
+        }
+    }
+}
