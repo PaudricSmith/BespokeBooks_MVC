504af42c52ec71bfe53d3c646012f891928d4d4a
Paudric Smith <75310103+PaudricSmith@users.noreply.github.com>
2024-04-04 15:20:37 +0100

    Added Upsert actions to the Product Controller instead of having Create and Edit actions and views.

--- BespokeWeb/Areas/Admin/Controllers/ProductController.cs
+++ BespokeWeb/Areas/Admin/Controllers/ProductController.cs
@@ -25,7 +25,7 @@ namespace BespokeBooksWeb.Areas.Admin.Controllers
             return View(objProductList);
         }
 
-        public IActionResult Create()
+        public IActionResult Upsert(int? id)
         {
             ProductVM productVM = new()
             {
@@ -39,11 +39,22 @@ namespace BespokeBooksWeb.Areas.Admin.Controllers
                 Product = new Product()
             };
 
-            return View(productVM);
+            if (id == null || id == 0)
+            {
+                // Create
+                return View(productVM);
+            }
+            else
+            {
+                // Update
+                productVM.Product = _unitOfWork.ProductRepo.Get(p => p.Id == id);
+                return View(productVM);
+            }
+
         }
 
         [HttpPost]
-        public IActionResult Create(ProductVM productVM)
+        public IActionResult Upsert(ProductVM productVM, IFormFile? file)
         {
             if (ModelState.IsValid)
             {
@@ -65,38 +76,6 @@ namespace BespokeBooksWeb.Areas.Admin.Controllers
 
                 return View(productVM);
             }
-        }
-
-        public IActionResult Edit(int? id)
-        {
-            if (id == null || id == 0)
-            {
-                return NotFound();
-            }
-
-            Product productFromDb = _unitOfWork.ProductRepo.Get(p => p.Id == id);
-            if (productFromDb == null)
-            {
-                return NotFound();
-            }
-
-            return View(productFromDb);
-        }
-
-        [HttpPost]
-        public IActionResult Edit(Product product)
-        {
-            if (ModelState.IsValid)
-            {
-                _unitOfWork.ProductRepo.Update(product);
-                _unitOfWork.Save();
-
-                TempData["Success"] = "Product updated successfully!";
-
-                return RedirectToAction("Index");
-            }
-
-            return View();
         }
 
         public IActionResult Delete(int? id)
