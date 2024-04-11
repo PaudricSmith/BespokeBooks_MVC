d013bd06b4692a0069541d1079d353edf5c4e487
Paudric Smith <75310103+PaudricSmith@users.noreply.github.com>
2024-04-11 14:41:43 +0100

    Added the product list to the home page which will be displayed to everyone.

--- BespokeWeb/Areas/Customer/Controllers/HomeController.cs
+++ BespokeWeb/Areas/Customer/Controllers/HomeController.cs
@@ -1,3 +1,4 @@
+using BespokeBooks.DataAccess.Repository.IRepository;
 using BespokeBooks.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
@@ -8,15 +9,18 @@ namespace BespokeBooksWeb.Areas.Customer.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IUnitOfWork _unitOfWork;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IUnitOfWork unitOfWork)
         {
             _logger = logger;
+            _unitOfWork = unitOfWork;
         }
 
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Product> productList = _unitOfWork.ProductRepo.GetAll(includeProperties: "Category");
+            return View(productList);
         }
 
         public IActionResult Privacy()
