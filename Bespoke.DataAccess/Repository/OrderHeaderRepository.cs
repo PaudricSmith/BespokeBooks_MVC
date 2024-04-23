f6ebbe8514ad601231a8342cbaa876b3b7091bce
Paudric Smith <75310103+PaudricSmith@users.noreply.github.com>
2024-04-23 12:15:18 +0100

    Added Stripe payment to App.

--- Bespoke.DataAccess/Repository/OrderHeaderRepository.cs
+++ Bespoke.DataAccess/Repository/OrderHeaderRepository.cs
@@ -19,5 +19,32 @@ namespace BespokeBooks.DataAccess.Repository
         {
             _dbContext.OrderHeaders.Update(orderHeader);
         }
-    }
+
+		public void UpdateStatus(int id, string orderStatus, string? paymentStatus = null)
+		{
+			var orderFromDb = _dbContext.OrderHeaders.FirstOrDefault(o => o.Id == id);
+            if (orderFromDb != null) 
+            {
+                orderFromDb.OrderStatus = orderStatus;
+                if (!string.IsNullOrEmpty(paymentStatus)) 
+                {
+                    orderFromDb.PaymentStatus = paymentStatus;
+                }
+            }
+		}
+
+		public void UpdateStripePaymentID(int id, string sessionId, string? paymentIntentId)
+		{
+			var orderFromDb = _dbContext.OrderHeaders.FirstOrDefault(o => o.Id == id);
+		    if (!string.IsNullOrEmpty(sessionId)) 
+            {
+                orderFromDb.SessionId = sessionId;
+            }
+            if (!string.IsNullOrEmpty(paymentIntentId))
+            {
+                orderFromDb.PaymentIntentId = paymentIntentId;
+                orderFromDb.PaymentDate = DateTime.Now;
+            }
+        }
+	}
 }
--- BespokeWeb/Areas/Customer/Controllers/CartController.cs
+++ BespokeWeb/Areas/Customer/Controllers/CartController.cs
@@ -1,8 +1,10 @@
 ﻿using BespokeBooks.DataAccess.Repository.IRepository;
 using BespokeBooks.Models;
 using BespokeBooks.Models.ViewModels;
+using BespokeBooks.Utility;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Stripe.Checkout;
 using System.Security.Claims;
 
 namespace BespokeBooksWeb.Areas.Customer.Controllers
@@ -13,6 +15,7 @@ namespace BespokeBooksWeb.Areas.Customer.Controllers
     {
         private readonly IUnitOfWork _unitOfWork;
 
+        [BindProperty]
         public ShoppingCartVM ShoppingCartVM { get; set; }
 
         public CartController(IUnitOfWork unitOfWork)
@@ -71,7 +74,128 @@ namespace BespokeBooksWeb.Areas.Customer.Controllers
             return View(ShoppingCartVM);
         }
 
-        public IActionResult Plus(int cartId)
+        [HttpPost]
+        [ActionName("Summary")]
+		public IActionResult SummaryPOST()
+		{
+			var claimsIdentity = (ClaimsIdentity)User.Identity;
+			var userId = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier).Value;
+
+            ShoppingCartVM.ShoppingCartList = _unitOfWork.ShoppingCartRepo
+                .GetAll(s => s.ApplicationUserId == userId, includeProperties: "Product");
+
+            ShoppingCartVM.OrderHeader.OrderDate = System.DateTime.Now;
+            ShoppingCartVM.OrderHeader.ApplicationUserId = userId;
+            ApplicationUser applicationUser = _unitOfWork.ApplicationUserRepo.Get(a => a.Id == userId);
+
+			foreach (var cart in ShoppingCartVM.ShoppingCartList)
+			{
+				cart.Price = GetPriceBasedOnQuantity(cart);
+				ShoppingCartVM.OrderHeader.OrderTotal += cart.Price * cart.Count;
+			}
+
+            if (applicationUser.CompanyId.GetValueOrDefault() == 0)
+            {
+                // It is a regular customer 
+                ShoppingCartVM.OrderHeader.PaymentStatus = SD.PaymentStatusPending;
+                ShoppingCartVM.OrderHeader.OrderStatus = SD.StatusPending;
+            }
+            else
+            {
+                // It is a company user
+                ShoppingCartVM.OrderHeader.PaymentStatus = SD.PaymentStatusDelayedPayment;
+                ShoppingCartVM.OrderHeader.OrderStatus = SD.StatusApproved;
+            }
+
+            _unitOfWork.OrderHeaderRepo.Add(ShoppingCartVM.OrderHeader);
+            _unitOfWork.Save();
+
+			foreach (var cart in ShoppingCartVM.ShoppingCartList)
+            {
+                OrderDetail orderDetail = new()
+                {
+                    ProductId = cart.ProductId,
+                    OrderHeaderId = ShoppingCartVM.OrderHeader.Id,
+                    Price = cart.Price,
+                    Count = cart.Count
+                };
+                _unitOfWork.OrderDetailRepo.Add(orderDetail);
+                _unitOfWork.Save();
+            }
+
+			if (applicationUser.CompanyId.GetValueOrDefault() == 0)
+			{
+				// It is a regular customer account and we need to capture payment
+				// Stripe logic
+                var domain = "https://localhost:7103/";
+                var options = new SessionCreateOptions
+                {
+                    SuccessUrl = domain + $"customer/cart/OrderConfirmation?id={ShoppingCartVM.OrderHeader.Id}",
+                    CancelUrl = domain + $"customer/cart/index",
+                    LineItems = new List<SessionLineItemOptions>(),
+					Mode = "payment",
+				};
+
+                foreach (var item in ShoppingCartVM.ShoppingCartList) 
+                {
+                    var sessionLineItem = new SessionLineItemOptions
+                    {
+                        PriceData = new SessionLineItemPriceDataOptions
+                        {
+                            UnitAmount = (long)(item.Price * 100), // $20.50 => 2050
+                            Currency = "eur",
+                            ProductData = new SessionLineItemPriceDataProductDataOptions
+                            {
+                                Name = item.Product.Title
+                            }
+                        },
+                        Quantity = item.Count
+                    };
+                    options.LineItems.Add(sessionLineItem);
+                }
+
+                var service = new SessionService();
+                Session session = service.Create(options);
+
+                _unitOfWork.OrderHeaderRepo.UpdateStripePaymentID(ShoppingCartVM.OrderHeader.Id, session.Id, session.PaymentIntentId);
+                _unitOfWork.Save();
+                
+                Response.Headers.Add("Location", session.Url);
+
+                return new StatusCodeResult(303);
+            }
+
+			return RedirectToAction(nameof(OrderConfirmation), new { id = ShoppingCartVM.OrderHeader.Id});
+		}
+
+        public IActionResult OrderConfirmation(int id)
+        {
+            OrderHeader orderHeader = _unitOfWork.OrderHeaderRepo.Get(o => o.Id == id, includeProperties: "ApplicationUser");
+            if (orderHeader.PaymentStatus != SD.PaymentStatusDelayedPayment)
+            {
+                // This is an order by Customer
+
+                var service = new SessionService();
+                Session session = service.Get(orderHeader.SessionId);
+
+                if (session.PaymentStatus.ToLower() == "paid")
+                {
+                    _unitOfWork.OrderHeaderRepo.UpdateStripePaymentID(id, session.Id, session.PaymentIntentId);
+                    _unitOfWork.OrderHeaderRepo.UpdateStatus(id, SD.StatusApproved, SD.PaymentStatusApproved);
+                    _unitOfWork.Save();
+                }
+            }
+
+            List<ShoppingCart> shoppingCarts = _unitOfWork.ShoppingCartRepo
+                .GetAll(s => s.ApplicationUserId == orderHeader.ApplicationUserId).ToList();
+            
+            _unitOfWork.ShoppingCartRepo.RemoveRange(shoppingCarts);
+            _unitOfWork.Save();
+
+            return View(id);
+        }
+
+		public IActionResult Plus(int cartId)
         {
             var cartFromDb = _unitOfWork.ShoppingCartRepo.Get(s => s.Id == cartId);
             cartFromDb.Count += 1;
