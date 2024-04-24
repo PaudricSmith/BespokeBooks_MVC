736b07b87d185a5b8037490f8bdcbaf9c71f06f6
Paudric Smith <75310103+PaudricSmith@users.noreply.github.com>
2024-04-24 13:50:54 +0100

    Added an OrderController and View that can be filtered by Status.

---
+++ BespokeWeb/Areas/Admin/Controllers/OrderController.cs
@@ -0,0 +1,57 @@
+﻿using BespokeBooks.DataAccess.Repository.IRepository;
+using BespokeBooks.Models;
+using BespokeBooks.Utility;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace BespokeBooksWeb.Areas.Admin.Controllers
+{
+	[Area("admin")]
+	public class OrderController : Controller
+	{
+		private readonly IUnitOfWork _unitOfWork;
+
+        public OrderController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+
+        public IActionResult Index()
+		{
+			return View();
+		}
+
+
+		#region API CALLS
+
+		[HttpGet]
+		public IActionResult GetAll(string status)
+		{
+			IEnumerable<OrderHeader> orderHeaderList = _unitOfWork.OrderHeaderRepo.GetAll(includeProperties: "ApplicationUser").ToList();
+			
+			switch (status)
+			{
+                case "pending":
+                    orderHeaderList = orderHeaderList.Where(o => o.PaymentStatus == SD.PaymentStatusDelayedPayment);
+                    break;
+                case "inprocess":
+                    orderHeaderList = orderHeaderList.Where(o => o.OrderStatus == SD.StatusInProcess);
+                    break;
+                case "completed":
+                    orderHeaderList = orderHeaderList.Where(o => o.OrderStatus == SD.StatusShipped);
+                    break;
+                case "approved":
+                    orderHeaderList = orderHeaderList.Where(o => o.OrderStatus == SD.StatusApproved);
+                    break;
+				default:
+					break;
+            }
+			
+			return Json(new { data = orderHeaderList });
+		}
+
+		#endregion
+	}
+}
