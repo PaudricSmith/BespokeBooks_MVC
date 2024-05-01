c99775f5ae91414627923c180b50730cf60b39dc
Paudric Smith <75310103+PaudricSmith@users.noreply.github.com>
2024-05-01 15:16:53 +0100

    Added user management where you can see all application users and lock or unlock a user.

---
+++ BespokeWeb/Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,82 @@
+﻿using BespokeBooks.Models;
+using BespokeBooks.DataAccess.Repository.IRepository;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using BespokeBooks.Utility;
+using BespokeBooks.DataAccess.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace BespokeBooksWeb.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = SD.Role_Admin)]
+    public class UserController : Controller
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public UserController(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        #region API CALLS
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            List<ApplicationUser> userLIst = _dbContext.ApplicationUsers.Include(a => a.Company).ToList();
+            var userRoles = _dbContext.UserRoles.ToList();
+            var roles = _dbContext.Roles.ToList();
+
+            foreach (var user in userLIst)
+            {
+                var roleId = userRoles.FirstOrDefault(u => u.UserId == user.Id).RoleId;
+                user.Role = roles.FirstOrDefault(r => r.Id == roleId).Name;
+
+                if (user.Company == null)
+                {
+                    user.Company = new Company() 
+                    { 
+                        Name = "" 
+                    };
+                }
+            }
+
+            return Json(new { data = userLIst });
+        }
+
+        [HttpPost]
+        public IActionResult LockUnlock([FromBody]string id)
+        {
+            var objFromDb = _dbContext.ApplicationUsers.FirstOrDefault(u => u.Id == id);
+            if (objFromDb == null)
+            {
+                return Json(new { success = false, message = "Error while Locking/Unlocking!" });
+            }
+
+            if (objFromDb.LockoutEnd != null && objFromDb.LockoutEnd > DateTime.Now)
+            {
+                // User is currently locked out and we need to unlock them
+                objFromDb.LockoutEnd = DateTime.Now;
+            }
+            else
+            {
+                objFromDb.LockoutEnd = DateTime.Now.AddYears(1000);
+            }
+
+            _dbContext.SaveChanges();
+
+            return Json(new { success = true, message = "Operation successfully!" });
+        }
+
+        #endregion
+
+    }
+}
